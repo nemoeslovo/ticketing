77f435ceadd301a6002ce11e8945ead25f2b32ee
Danila Kolesnikov <nitropolis@gmail.com>
2020-07-02 10:22:54 +0700

    signup flow

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,9 +9,17 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +33,9 @@ app.all("*", async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
   try {
     await mangoose.connect("mongodb://auth-mongo-srv:27017/auth", {
       useNewUrlParser: true,
