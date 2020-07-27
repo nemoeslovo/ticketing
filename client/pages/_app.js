703abb4e0602ea61f8351d2347da91a536b4793b
Danila Kolesnikov <nitropolis@gmail.com>
2020-07-27 14:16:31 +0700

    mix static and initial props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,21 @@
 import "./sb-admin-2.min.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const { data } = await buildClient(appContext.ctx).get(
+    "api/users/currentuser"
+  );
+  return { ...data };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,7 @@
 import buildClient from "../api/build-client";
 
-const LandingPage = ({ data: { currentUser } }) => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   const msg = currentUser ? "You are signed in" : "You are not signin";
   return <h1>{msg}</h1>;
 };
@@ -9,5 +10,7 @@ export default LandingPage;
 
 export const getServerSideProps = async (context) => {
   const { data } = await buildClient(context).get("api/users/currentuser");
-  return { props: { data } };
+  const result = { props: { ...data } };
+  console.log(result);
+  return result;
 };
