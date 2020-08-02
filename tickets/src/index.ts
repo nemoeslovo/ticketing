5be2b4d32cfb4d00c7a6131d57d47b38831c58a6
Danila Kolesnikov <nitropolis@gmail.com>
2020-08-02 17:47:53 +0700

    infra for a new service

---
+++ tickets/src/index.ts
@@ -0,0 +1,23 @@
+import mangoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  try {
+    await mangoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("connected to mongo");
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!");
+  });
+};
+
+start();
