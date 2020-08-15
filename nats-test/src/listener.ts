61dcb7827b9f3e8adcf02d69ec29ad8d07d60bac
Danila Kolesnikov <nitropolis@gmail.com>
2020-08-15 17:26:17 +0700

    generic event

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,21 @@
 import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
-const stan = nats.connect("ticketing", "client", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
-
-  subscription.on("message", (msg) => {
-    console.log("Message recieved");
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
   });
+
+  new TicketCreatedListener(stan).listen();
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
