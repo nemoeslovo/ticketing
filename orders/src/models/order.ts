4282a8c4585cded371e302d316d52383a8d42348
Danila Kolesnikov <nitropolis@gmail.com>
2020-09-01 09:21:18 +0700

    initial new route

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { OrderStatus } from "@capitanyo/common";
 import { TicketDoc } from "./ticket";
 
+export { OrderStatus };
+
 interface OrdersAttrs {
   userId: string;
   status: OrderStatus;
